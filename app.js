44957b75061631127d0c04a39fc91b8c3d281003
Chamon562 <Chamon562@gmail.com>
2021-01-31 23:58:23 -0800

    wrote function that takes 2 parameters to add returend it and console.log the addAnswer.

---
+++ app.js
@@ -0,0 +1,9 @@
+// creating a function that adds 2 numebrs
+// need 2 parameters for num1 and num2 and inside the function make both numbers add
+
+addNumbers = (num1, num2) =>{
+   return num1 + num2
+}
+
+let addAnswer = addNumbers(2, 3);
+console.log(addAnswer)
