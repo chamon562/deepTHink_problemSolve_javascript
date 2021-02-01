e5a284de3392f8768cc22c72eb61389fda800c86
Chamon562 <Chamon562@gmail.com>
2021-02-01 00:21:40 -0800

    turning the number into a string by making a variable calling the function with 2 numbers and logging it, then making another variable called turnstring and made it take the addAnswer variable using .toString to turn the integer into a string shown indicated in black color if string, and blue says it s anumber.

--- app.js
+++ app.js
@@ -6,4 +6,15 @@ addNumbers = (num1, num2) =>{
 }
 
 let addAnswer = addNumbers(2, 3);
-console.log(addAnswer)
+console.log(addAnswer) //blue 5
+let turnString = addAnswer.toString();
+// if the number is black its a string using black 5
+console.log("this is turning into string with toString", turnString);
+function add(num1, num2){
+    return num1 + num2 
+}
+// if the number is blue, its a number
+console.log(add(1, 3))
+console.log(add(5, 3))
+console.log(add(9, 3))
+console.log(add(10, 3))
