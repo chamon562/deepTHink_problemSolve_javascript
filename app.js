2f49eb0638def854181bba9c7769153afad4eb71
Chamon562 <Chamon562@gmail.com>
2021-02-01 00:29:56 -0800

    Add minToSeconds function took in minutes as the parameter and retun minutes multiplied by 60 and logged the function minToSeconds with an integer of 1 and 3 to get it in seconds.

--- app.js
+++ app.js
@@ -6,15 +6,28 @@ addNumbers = (num1, num2) =>{
 }
 
 let addAnswer = addNumbers(2, 3);
-console.log(addAnswer) //blue 5
+// console.log(addAnswer) //blue 5
 let turnString = addAnswer.toString();
 // if the number is black its a string using black 5
-console.log("this is turning into string with toString", turnString);
+// console.log("this is turning into string with toString", turnString);
 function add(num1, num2){
     return num1 + num2 
 }
 // if the number is blue, its a number
-console.log(add(1, 3))
-console.log(add(5, 3))
-console.log(add(9, 3))
-console.log(add(10, 3))
+// console.log(add(1, 3))
+// console.log(add(5, 3))
+// console.log(add(9, 3))
+// console.log(add(10, 3))
+
+// Converting minutes to seconds.
+// lets say 1 minute. and it equeals 60 seconds. 
+// make function that converts minutes to seconds
+// takes in minutes 
+
+function minToSeconds(minutes){
+    return minutes * 60 ;
+}
+let minInSeconds = minToSeconds(1);
+console.log(minInSeconds);
+console.log(`1 minute converted into seconds is: ${minToSeconds(1)}`)
+console.log(minToSeconds(3));
